7be99cd384975a3ebb5536fef57fd9acad182305
jan_hulanicki@poczta.onet.pl <jan.hulanicki@poczta.onet.pl>
2019-09-28 20:08:19 +0200

    First Commit

---
+++ src/main/java/com/kodilla/kodillacoursecalcul/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacoursecalcul;
+
+public class Calculator {
+    public void add(){
+        System.out.println("It works!");
+    }
+    public static void main(String args[]){
+        Calculator calculator=new Calculator();
+        calculator.add();
+    }
+
+
+}
